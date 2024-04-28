ad2da5cedcebf3b774d786e42e56d5e40aaa6b9c
KevinOrellana07 <135291944+KevinOrellana07@users.noreply.github.com>
2024-04-27 22:37:57 -0600

    Add files via upload

---
+++ Calculadoraviajes.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora_de_viajes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void FormularioPrincipal_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CalcularTiempoBtn_Click(object sender, EventArgs e)
+        {
+            double distancia = double.Parse(TextDistancia.Text);
+            double velocidad = double.Parse(TextVelocidad.Text);
+
+            double tiempoEstimado = distancia / velocidad;
+
+            MessageBox.Show("Hola, el tiempo estimado de viaje es " + tiempoEstimado + " horas");
+        }
+
+        private void TextDistancia_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextVelocidad_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void BotonVacio_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
---
+++ Combustible.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace combustible
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void IniciarCalculo_Click(object sender, EventArgs e)
+        {
+            double distanciaRecorrida = double.Parse(TextDistancia.Text);
+            double rendimiento = double.Parse(TextRendimiento.Text);
+
+            double resultado = distanciaRecorrida / rendimiento;
+
+            TextTotal.Text = resultado.ToString();
+        }
+
+        private void EtiquetaMensaje_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
